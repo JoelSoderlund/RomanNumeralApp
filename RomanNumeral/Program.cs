b74fc6c6b9dc4e3cec0832e4ec56747dcd997cbe
Emma Persson <persson.emma99@outlook.com>
2024-10-09 14:29:50 +0200

    DONE

--- RomanNumeral/Program.cs
+++ RomanNumeral/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.ComponentModel.Design;
+using System.Text.RegularExpressions;
 
 namespace RomanNumeral
 {
@@ -7,15 +8,16 @@ namespace RomanNumeral
         static void Main(string[] args)
         {
             bool isValidInput = false;
+            var userInput = "";
 
             while (!isValidInput) //Kontrollerar så att endast skriv in romserska siffror
             {
-
+                Console.Clear();
                 Console.WriteLine("Ange ett Romerskt tal: ");
-                var userInput = Console.ReadLine().ToUpper();
+                userInput = Console.ReadLine().ToUpper();
 
                 char[] checkInput = userInput.ToCharArray();
-                
+
                 foreach (char c in checkInput)
                 {
                     if (userInput.Length <= 15 && c == 'I' || c == 'V' || c == 'X' || c == 'L' || c == 'C' || c == 'D' || c == 'M')
@@ -25,11 +27,12 @@ namespace RomanNumeral
                     else
                     {
                         isValidInput = false;
+                        Console.WriteLine("Felaktig användarinmatning! Försök igen LOOOOOSEEEEERRRRR");
+                        Thread.Sleep(1500);
                         break;
                     }
                 }
             }
-
 
             var romanNums = new Dictionary<char, int>
             {
@@ -41,6 +44,19 @@ namespace RomanNumeral
                 {'D', 500},
                 {'M', 1000}
             };
+
+            int result = 0;
+
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                if (i + 1 < userInput.Length && romanNums[userInput[i]] < romanNums[userInput[i +1]])
+                    result -= romanNums[userInput[i]];
+                else
+                    result += romanNums[userInput[i]];
+            }
+            Console.WriteLine($"Talet är {result}");
+                 
+
 
 
 
